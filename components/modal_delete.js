cd2bed172ce5d4a68f66b39ead775e01a0310003
thiagobachiega <99752145+thiagobachiega@users.noreply.github.com>
2022-05-17 18:36:34 -0300

    Alinhamento do modal

--- components/modal_delete.js
+++ components/modal_delete.js
@@ -1,15 +1,17 @@
 const Modal = ( { modalConfirm } ) => {
   return(
-    <div className="border-2 rounded-md border-black w-96 m-auto">
-      <div className="flex flex-col text-center space-y-5">
-        <div className="border-b-2 border-black">
-          <h1 className="font-bold text-lg">Tem certeza que deseja excluir este carro?</h1>
+    <div className="w-screen h-screen fixed top-0 left-0 z-2000 flex">
+      <div className="border-2 rounded-md border-black w-96 m-auto">
+        <div className="flex flex-col text-center space-y-5 bg-white">
+          <div className="border-b-2 border-black bg-black text-white">
+            <h1 className="font-bold text-lg">Tem certeza que deseja excluir este carro?</h1>
+          </div>
+          <p>Essa ação não poderá ser desfeita.</p>
         </div>
-        <p>Essa ação não poderá ser desfeita.</p>
-      </div>
-      <div className="flex text-center mt-10 border-t-2 border-black">
-        <div className="border-r-2 border-black w-1/2"><button onClick={() => {modalConfirm(false)}}>Não</button></div>
-        <div className="w-1/2 hover:bg-red-500"><button onClick={() => {modalConfirm(true)}}>Sim</button></div>
+        <div className="flex text-center mt-10 border-t-2 border-black">
+          <div className="border-r-2 border-black w-1/2"><button onClick={() => {modalConfirm(false)}}>Não</button></div>
+          <div className="w-1/2 hover:bg-red-500"><button onClick={() => {modalConfirm(true)}}>Sim</button></div>
+        </div>
       </div>
     </div>
   )
